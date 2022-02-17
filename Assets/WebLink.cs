ada064f573d768ca4fa992011095be5266cc4137
Sagar Suman <sagarsuman1299@gmail.com>
2022-02-17 13:36:33 +0530

    assets

---
+++ Assets/WebLink.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WebLink : MonoBehaviour
+{
+    public void SagarFb()
+    {
+        Application.OpenURL("https://www.facebook.com/sagarsuman1299");
+    }
+
+    public void AshishFb()
+    {
+        Application.OpenURL("https://www.facebook.com/profile.php?id=100030357640845");
+    }
+    public void AvanthyFb()
+    {
+        Application.OpenURL("https://www.facebook.com/avanthy.yeluri");
+    }
+    public void AkshayFb()
+    {
+        Application.OpenURL("https://www.facebook.com/akshay.manoj.37");
+    }
+    public void Sagarln()
+    {
+        Application.OpenURL("https://www.linkedin.com/in/sagar-suman-66974b199/");
+    }
+
+    public void Ashishln()
+    {
+        //Application.OpenURL("https://www.linkedin.com/in/ashish-sinha-69b151171");
+    }
+    public void Avanthyln()
+    {
+       // Application.OpenURL("https://www.facebook.com/avanthy.yeluri");
+    }
+    public void Akshayln()
+    {
+        //Application.OpenURL("https://www.linkedin.com/in/akshay-manoj-bhaskaran-404315192");
+    }
+
+
+
+}
---
+++ Assets/aniimalGame/RandomizeAnimals.cs
@@ -0,0 +1,287 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using System.IO;
+
+public class RandomizeAnimals : MonoBehaviour
+{
+    int t,k=0;
+    public GameObject[] Ques;
+    public Text HeadingText, BottomText;
+    private string temporaryText;
+    private string WildText = "Choose a Wild Animal";
+    private string DomesticText = "Choose a Domestic Animal";
+    public AudioSource wildSound;
+    public AudioSource domesticSound;
+    private void Start()
+    {
+        NumberRandomise();
+
+    }
+    private void Update()
+    {
+        //  if(temporaryText==)
+    }
+    public void Elephant()
+    {
+        temporaryText = WildText;
+        if (HeadingText.text == temporaryText)
+        {
+            BottomText.text = "Correct! Now Try The Next One";
+            BottomText.color = Color.green;
+            StartCoroutine(WaitForTime());
+            NumberRandomise();
+        }
+        else
+        {
+            BottomText.text = "Incorrect Please Try Again";
+            BottomText.color = Color.red;
+            StartCoroutine(WaitForTime());
+        }
+    }
+    public void Dog()
+    {
+        temporaryText = DomesticText;
+        if (HeadingText.text == temporaryText)
+        {
+            BottomText.text = "Correct Now Try The Next One";
+            BottomText.color = Color.green;
+            StartCoroutine(WaitForTime());
+            NumberRandomise();
+        }
+        else
+        {
+            BottomText.text = "Incorrect Please Try Again";
+            BottomText.color = Color.red;
+            StartCoroutine(WaitForTime());
+        }
+    }
+    public void Cow()
+    {
+        temporaryText = DomesticText;
+        if (HeadingText.text == temporaryText)
+        {
+            BottomText.text = "Correct Now Try The Next One";
+            BottomText.color = Color.green;
+            StartCoroutine(WaitForTime());
+            NumberRandomise();
+        }
+        else
+        {
+            BottomText.text = "Incorrect Please Try Again";
+            BottomText.color = Color.red;
+            StartCoroutine(WaitForTime());
+        }
+    }
+    public void Goat()
+    {
+        temporaryText = DomesticText;
+        if (HeadingText.text == temporaryText)
+        {
+            BottomText.text = "Correct Now Try The Next One";
+            BottomText.color = Color.green;
+            StartCoroutine(WaitForTime());
+            NumberRandomise();
+        }
+        else
+        {
+            BottomText.text = "Incorrect Please Try Again";
+            BottomText.color = Color.red;
+            StartCoroutine(WaitForTime());
+        }
+    }
+    public void Giraffe()
+    {
+        temporaryText = WildText;
+        if (HeadingText.text == temporaryText)
+        {
+            BottomText.text = "Correct Now Try The Next One";
+            BottomText.color = Color.green;
+            StartCoroutine(WaitForTime());
+            NumberRandomise();
+        }
+        else
+        {
+            BottomText.text = "Incorrect Please Try Again";
+            BottomText.color = Color.red;
+            StartCoroutine(WaitForTime());
+        }
+    }
+    public void Horse()
+    {
+        temporaryText = DomesticText;
+        if (HeadingText.text == temporaryText)
+        {
+            BottomText.text = "Correct Now Try The Next One";
+            BottomText.color = Color.green;
+            StartCoroutine(WaitForTime());
+            NumberRandomise();
+        }
+        else
+        {
+            BottomText.text = "Incorrect Please Try Again";
+            BottomText.color = Color.red;
+            StartCoroutine(WaitForTime());
+        }
+    }
+    public void Rhino()
+    {
+        temporaryText = WildText;
+        if (HeadingText.text == temporaryText)
+        {
+            BottomText.text = "Correct Now Try The Next One";
+            BottomText.color = Color.green;
+            StartCoroutine(WaitForTime());
+            NumberRandomise();
+        }
+        else
+        {
+            BottomText.text = "Incorrect Please Try Again";
+            BottomText.color = Color.red;
+            StartCoroutine(WaitForTime());
+        }
+    }
+    public void Zebra()
+    {
+        temporaryText = WildText;
+        if (HeadingText.text == temporaryText)
+        {
+            BottomText.text = "Correct Now Try The Next One";
+            BottomText.color = Color.green;
+            StartCoroutine(WaitForTime());
+            NumberRandomise();
+        }
+        else
+        {
+            BottomText.text = "Incorrect Please Try Again";
+            BottomText.color = Color.red;
+
+            StartCoroutine(WaitForTime());
+        }
+    }
+    public void Tiger()
+    {
+        temporaryText = WildText;
+        if (HeadingText.text == temporaryText)
+        {
+            BottomText.text = "Correct Now Try The Next One";
+            BottomText.color = Color.green;
+            StartCoroutine(WaitForTime());
+            NumberRandomise();
+        }
+        else
+        {
+            BottomText.text = "Incorrect Please Try Again";
+            BottomText.color = Color.red;
+            StartCoroutine(WaitForTime());
+        }
+    }
+    public void Lion()
+    {
+        temporaryText = WildText;
+        if (HeadingText.text == temporaryText)
+        {
+            BottomText.text = "Correct Now Try The Next One";
+
+            BottomText.color = Color.green;
+            StartCoroutine(WaitForTime());
+            NumberRandomise();
+        }
+        else
+        {
+            BottomText.text = "Incorrect Please Try Again";
+            BottomText.color = Color.red;
+            StartCoroutine(WaitForTime());
+        }
+    }
+
+
+    public void NumberRandomise()
+    {
+        t = Random.Range(1, 7);
+        if(k==t)
+        {
+            t = Random.Range(1, 7);
+        }
+        if (t == 1)
+        {
+            wildSound.Play();
+            HeadingText.text = WildText;
+            Ques[0].SetActive(true);
+            Ques[1].SetActive(false);
+            Ques[2].SetActive(false);
+            Ques[3].SetActive(false);
+            Ques[4].SetActive(false);
+            Ques[5].SetActive(false);
+            k=t;
+        }
+        if (t == 2)
+        {
+            domesticSound.Play();
+            HeadingText.text = DomesticText;
+            Ques[0].SetActive(false);
+            Ques[1].SetActive(true);
+            Ques[2].SetActive(false);
+            Ques[3].SetActive(false);
+            Ques[4].SetActive(false);
+            Ques[5].SetActive(false);
+            k=t;
+        }
+        if (t == 3)
+        {
+            domesticSound.Play();
+            HeadingText.text = DomesticText;
+            Ques[0].SetActive(false);
+            Ques[1].SetActive(false);
+            Ques[2].SetActive(true);
+            Ques[3].SetActive(false);
+            Ques[4].SetActive(false);
+            Ques[5].SetActive(false);
+            k = t;
+        }
+        if (t == 4)
+        {
+            domesticSound.Play();
+            HeadingText.text = DomesticText;
+            Ques[0].SetActive(false);
+            Ques[1].SetActive(false);
+            Ques[2].SetActive(false);
+            Ques[3].SetActive(true);
+            Ques[4].SetActive(false);
+            Ques[5].SetActive(false);
+            k = t;
+        }
+        if (t == 5)
+        {
+            wildSound.Play();
+            HeadingText.text = WildText;
+            Ques[0].SetActive(false);
+            Ques[1].SetActive(false);
+            Ques[2].SetActive(false);
+            Ques[3].SetActive(false);
+            Ques[4].SetActive(true);
+            Ques[5].SetActive(false);
+            k = t;
+        }
+        if (t == 6)
+        {
+            wildSound.Play();
+            HeadingText.text = WildText;
+            Ques[0].SetActive(false);
+            Ques[1].SetActive(false);
+            Ques[2].SetActive(false);
+            Ques[3].SetActive(false);
+            Ques[4].SetActive(false);
+            Ques[5].SetActive(true);
+            k = t;
+        }
+    }
+    public IEnumerator WaitForTime()
+    {
+        yield return new WaitForSeconds(2);
+        BottomText.text = "_______________";
+        BottomText.color = Color.black;
+    }
+
+}
